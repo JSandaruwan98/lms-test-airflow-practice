df0982d7535f2f6ef0c8923a48d0f65fa58dbb88
JSandaruwan98 <sandru98093@gmail.com>
2026-05-26 07:12:28 +0530

    created extract file

--- etl/extract/extract_students.py
+++ etl/extract/extract_students.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from config.db_config import LMS_DB_CONFIG
+from etl.config.db_config import LMS_DB_CONFIG
 
 def extract_student_data():
     conn_str = f"postgresql://{LMS_DB_CONFIG['user']}:{LMS_DB_CONFIG['password']}@{LMS_DB_CONFIG['host']}:{LMS_DB_CONFIG['port']}/{LMS_DB_CONFIG['database']}"
