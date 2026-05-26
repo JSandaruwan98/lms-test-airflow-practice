83eda9323c4a059e266d498503a73583af337390
JSandaruwan98 <sandru98093@gmail.com>
2026-05-26 07:23:28 +0530

    created extract file

--- etl/extract/extract_students.py
+++ etl/extract/extract_students.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from urllib.parse import quote_plus
 from etl.config.db_config import LMS_DB_CONFIG
 
 def extract_student_data():
-    conn_str = f"postgresql://{LMS_DB_CONFIG['user']}:{LMS_DB_CONFIG['password']}@{LMS_DB_CONFIG['host']}:{LMS_DB_CONFIG['port']}/{LMS_DB_CONFIG['database']}"
+    # Encode the password to handle special characters like '@'
+    user = LMS_DB_CONFIG['user']
+    password = quote_plus(LMS_DB_CONFIG['password']) # <--- Encode here
+    host = LMS_DB_CONFIG['host']
+    port = LMS_DB_CONFIG['port']
+    db = LMS_DB_CONFIG['database']
+
+    conn_str = f"postgresql://{user}:{password}@{host}:{port}/{db}"
     engine = create_engine(conn_str)
     
     query = "SELECT * FROM students" # Assuming students table exists
