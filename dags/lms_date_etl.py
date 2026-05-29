f8760b66bf322c65ddbbf3f48120f31217a64f46
JSandaruwan98 <sandru98093@gmail.com>
2026-05-29 08:29:47 +0530

    dim_date loaded

--- dags/lms_date_etl.py
+++ dags/lms_date_etl.py
@@ -1,17 +1,17 @@
+import sys
+import os
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import sys
-import os
 
 # Adjust path to find your ETL scripts
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-def load_date_task():
-    from etl.load.load_date import load_dim_date
-    load_dim_date()
+def extract_task_callable():
+    from etl.extract.extract_course import extract_course_data
+    return extract_course_data()
 
 default_args = {
     'owner': 'airflow',
@@ -22,18 +22,18 @@ default_args = {
 }
 
 with DAG(
-    dag_id='lms_dim_date_loader',
+    dag_id='lms_date_v2',
     default_args=default_args,
     description='Populates the static Date Dimension',
-    schedule='@once',  # You only need to run this once
+    schedule='@daily',  # You only need to run this once
     start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=['lms', 'date'],
 ) as dag:
+    
+    t1 = PythonOperator(
+        task_id='extract_courses',
+        python_callable=extract_task_callable
+    )
 
-    # t1 = PythonOperator(
-    #     task_id='populate_dim_date',
-    #     python_callable=load_date_task
-    # )
-
-    # t1
+    t1
