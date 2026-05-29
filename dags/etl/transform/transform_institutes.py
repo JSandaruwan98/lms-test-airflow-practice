a748de8a1d6520c6761b42ccddc228cef27968a3
JSandaruwan98 <sandru98093@gmail.com>
2026-05-29 07:00:10 +0530

    institute transform task

--- dags/etl/transform/transform_institutes.py
+++ dags/etl/transform/transform_institutes.py
@@ -2,11 +2,11 @@ import pandas as pd
 import pandas as pd
 import os
 
-def transform_course_data(input_path):
+def transform_institute_data(input_path):
     """
-    input_path: The path to the extracted course data (JSON/Parquet/CSV)
+    input_path: The path to the extracted institute data (JSON/Parquet/CSV)
     """
-    print(f"Starting Course transformation. Input path: {input_path}")
+    print(f"Starting institute transformation. Input path: {input_path}")
 
     if not input_path or not os.path.exists(input_path):
         raise FileNotFoundError(f"Could not find input file: {input_path}")
@@ -19,23 +19,20 @@ def transform_course_data(input_path):
     else:
         df = pd.read_csv(input_path, on_bad_lines='skip')
 
-    # 2. Apply Course-Specific Transformations
+    # 2. Apply institute-Specific Transformations
     
     # Rename columns to match a Warehouse standard (optional but recommended)
-    # This maps 'id' to 'course_id' and 'name' to 'course_name'
+    # This maps 'id' to 'institute_id' and 'name' to 'institute_name'
     df = df.rename(columns={
-        'id': 'course_id',
-        'name': 'course_name'
+        'id': 'institute_id',
+        'name': 'institute_name',
+        'city': 'city',
     })
 
-    # Data Cleaning: Ensure price is not null
-    if 'price' in df.columns:
-        df['price'] = df['price'].fillna(0.0)
-
     # Add metadata for the ETL process
-    df['processed_at'] = pd.Timestamp.now()
+    df['start_date'] = pd.Timestamp.now()
     
-    # Add a 'current_flag' like we did for students (if your dim_course uses it)
+    # Add a 'current_flag' like we did for students (if your dim_institute uses it)
     df['current_flag'] = 'Y' 
 
     # 3. Save the transformed data 
@@ -43,7 +40,7 @@ def transform_course_data(input_path):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     root_dir = os.path.dirname(current_dir) # Goes up one level to root
 
-    output_path = os.path.join(root_dir, "course_transform_data.json")
+    output_path = os.path.join(root_dir, "institute_transform_data.json")
     
     print(f"Saving transformed data to: {output_path}")
     
