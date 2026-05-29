500f122c925c343d6cde6947508d193a227e53c3
JSandaruwan98 <sandru98093@gmail.com>
2026-05-29 08:17:25 +0530

    dim_date loaded

--- dags/lms_date_etl.py
+++ dags/lms_date_etl.py
@@ -15,6 +15,8 @@ def load_date_task():
 
 default_args = {
     'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
